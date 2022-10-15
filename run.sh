01d0a04deac08e918f4f3323b23c0d31ac2eeebe
“Jhon <j.gaviria@utp.edu.co>
2022-10-15 16:15:29 -0500

    Change in the output format

--- run.sh
+++ run.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-for VARIABLE in 10 100 1000 #10000 700000
+echo "Datos;TreeSort;HeapSort;MergeSort;QuickSort;Introsort;TimSort"
+for VARIABLE in 10 20 40 80 160 320 640 1280 2560 5120 10240 20480 40960 81920 163840 327680 655360
 do
     # Data Generator
-    echo "\n\nWith $VARIABLE data: "
     echo "$VARIABLE" > in
     g++ DataGenerator.cpp 
     ./a.out < in > input
 
-    for algorithm in MergeSort QuickSort Introsort
+    echo -n "$VARIABLE;"
+
+    for algorithm in TreeSort HeapSort MergeSort QuickSort Introsort TimSort
     do
 
-        echo "Sorting altgorithm: $algorithm"
+        # echo -n "Sorting altgorithm: $algorithm;"
         $(g++ C++/$algorithm.cpp)
         totaltime=0;
-        echo "c++ script"
+        # echo "c++ script"
         for i in $(seq 1 10)
         do
             start=`date +%s.%N`
@@ -26,11 +28,11 @@ do
         done
 
         runtime=$( echo "$totaltime / 10" | bc -l )
-        echo "0$runtime"
+        echo -n "0$runtime;"
 
         # Run Python3 Script
         totaltime=0;
-        echo "Pyhton3 script"
+        # echo "Pyhton3 script"
         for i in $(seq 1 10)
         do
             start=`date +%s.%N`
@@ -42,6 +44,7 @@ do
         done
 
         runtime=$( echo "$totaltime / 10" | bc -l )
-        echo "0$runtime"
+        echo -n "0$runtime;"
     done
+    echo ""
 done
